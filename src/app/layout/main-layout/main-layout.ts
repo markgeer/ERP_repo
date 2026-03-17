8a6035f55031ea998a68f11b6135c7a90b0a34c7
markgeer <marksmash3@gmail.com>
2026-03-17 15:36:02 -0600

    -> se modificaron estilos

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { Sidebar } from '../../components/sidebar/sidebar';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-main-layout',
   imports: [Sidebar, RouterOutlet],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
+
 export class MainLayout {}
